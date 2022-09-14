1629ca4c99f5674bc6542a0d08d1cfa61e7a512c
Mohammed-Ibrahim-15 <ibrahim72342@gmail.com>
2022-09-14 23:48:34 +0600

    error count added

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    errorCount++
   }
 
   // check if given question text is equal to user typed text
@@ -71,6 +72,7 @@ const gameOver = () => {
 
   // show result modal
   resultModal.innerHTML = "";
+
   resultModal.classList.toggle("hidden");
   modalBackground.classList.toggle("hidden");
   // clear user text
@@ -80,10 +82,12 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${parseInt(timeTaken)}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
+
+  resultModal.style.display = 'center';
 
   addHistory(questionText, timeTaken, errorCount);
 
